78cbb236b6b70285ac2f2f2a545c120c0f4387c0
Peter Elorm Seth Selormey <pesselormey@st.ug.edu.gh>
2025-08-13 23:48:25 +0000

    Add FinanceSystem.dll and refint for .NET 9.0 build
    - Generated new FinanceSystem.dll and reference assembly for .NET 9.0.
    - Included necessary metadata and assembly attributes.
    - Updated assembly identity and trust information in the manifest.

--- FinanceSystem/obj/Debug/net9.0/FinanceSystem.AssemblyInfo.cs
+++ FinanceSystem/obj/Debug/net9.0/FinanceSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d428041b1cd48d350975c66b06ed459b7c681d0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
